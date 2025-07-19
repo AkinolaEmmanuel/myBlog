f45921af1e9701e2fb6bbd45aadbfa264dbd822b
Emmanuel Akinola <emmanuelakinola255@gmail.com>
2025-07-20 00:10:46 +0100

    NewsLetter Update

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,4 +1,8 @@
+import { useEffect } from "react";
+
 export default function Newsletter() {
+
+ 
   return (
     <>
         <div className="flex flex-col xl:flex-row items-start lg:items-center justify-between gap-10 bg-black rounded-lg p-5 md:p-10 my-10">
@@ -6,10 +10,14 @@ export default function Newsletter() {
         <h1 className="font-bold text-3xl">Subscribe to my Newsletter.</h1>
         <p className="text-xl font-light">You can get my latest writings and thoughts delivered to your mailbox.</p>
         </div>
-        <div className="flex flex-col md:flex-row items-start md:items-center flex-1 gap-y-3 w-full max-w-xl">
-            <input type="email" placeholder="Enter your email" className="w-full p-2 rounded-md md:rounded-e-none border-2 border-black bg-slate-100 outline-none text-lg md:text-xl font-semibold text-black" />
+       
+          <form className="flex flex-col md:flex-row items-start md:items-center flex-1 gap-y-3 w-full max-w-xl" action="https://formsubmit.co/emmanuelakinola255@gmail.com" method="post">
+            <input type="email" name="email" placeholder="Enter your email" className="w-full p-2 rounded-md md:rounded-e-none border-2 border-black bg-slate-100 outline-none text-lg md:text-xl font-semibold text-black" />
+            <input type="text" name="name" value={"I am a new subscriber"} hidden />
+            <input type="hidden" name="_captcha" value="false"></input>
             <input type="submit" value="SUBSCRIBE" className="px-5 py-1 md:py-2 rounded-md md:rounded-s-none border-2 border-s-0 border-black bg-slate-400 font-medium text-lg md:text-xl" />
-        </div>
+            </form>
+        
         </div>
 
     </>
